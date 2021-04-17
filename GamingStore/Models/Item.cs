ebd9f05d4868effb30b16230695c3f0910dd801c
Nitzan Miranda <Nizzan943@gmail.com>
2021-04-17 20:31:46 +0300

    fu

---
+++ GamingStore/Models/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamingStore.Models
+{
+    public class Item
+    {
+
+        [Key]
+        public int ItemId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public float Price { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        public int StockCounter { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public Dictionary<string,string> PropertiesList { get; set; }
+        [Required]
+        public string Category { get; set; }
+        public float StarReview { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public bool Active { get; set; } = true;
+    }
+}
