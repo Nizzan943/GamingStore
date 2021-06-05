6f4aab06cda91e2ab93374bf7e82392d72fac41d
Nitzan Miranda <nizzan943@gmail.com>
2021-06-05 19:09:47 +0300

    category progress

--- src/GamingStore/Controllers/CategoriesController.cs
+++ src/GamingStore/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -35,11 +36,16 @@ namespace GamingStore.Controllers
 
             var category = await _context.Category
                 .FirstOrDefaultAsync(m => m.Id == id);
+            
             if (category == null)
             {
                 return NotFound();
             }
 
+            var items = new List<Item>();
+            items.AddRange(_context.Item.Where(i => i.CategoryId == id));
+
+            category.Items = items;
             //ViewData["ItemsList"] = _context.Item.Where(x => x.CategoryId == category.Id);
             return View(category);
         }
