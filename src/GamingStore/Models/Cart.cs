0710f216a4565fe62b7c593d834b57a08c882fc3
oriba <oribachar98@gmail.com>
2021-06-07 18:35:23 +0300

    Add cart model

---
+++ src/GamingStore/Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamingStore.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DataType(DataType.Text)]
+        public string UserId { get; set; }
+
+        public int Quantity { get; set; }
+
+        [NotMapped]
+        public Item Item { get; set; }
+    }
+}
