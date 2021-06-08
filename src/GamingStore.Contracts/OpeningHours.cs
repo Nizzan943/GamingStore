0f6015617251f33d163b998b5ac743695510b8f6
oriba <oribachar98@gmail.com>
2021-06-08 18:49:43 +0300

    Add opening hours class

---
+++ src/GamingStore.Contracts/OpeningHours.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GamingStore.Contracts
+{
+    public class OpeningHours
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+
+        [DataType(DataType.Time)]
+        public TimeSpan OpeningTime { get; set; }
+
+        [DataType(DataType.Time)]
+        public TimeSpan ClosingTime { get; set; }
+    }
+}
