164e26920052254b5382310a84edab9984f6444e
yuvalamir123 <yuval.amir998@gmail.com>
2021-06-16 18:06:36 +0300

    item search by brand

---
+++ src/GamingStore/wwwroot/js/search.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    /*
+    $('form').submit(function (e) {
+        e.preventDefault();
+
+        var brands = $('#brands').val();
+
+        $('#toload').load('/Item/SearchByBrand?brands=' + brands);
+    });
+    */
+
+});
