cd962e1cb07c89a5cccffe3adb4a4b75c6ed4f56
yuvalamir123 <yuval.amir998@gmail.com>
2021-08-12 19:19:48 +0300

    categories in admin icon

---
+++ src/GamingStore/ViewModels/Administration/ListCategoriesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using GamingStore.Models;
+
+namespace GamingStore.ViewModels.Administration
+{
+    public class ListCategoriesViewModel
+    {
+
+        public IEnumerable<Category> Categories { get; set; }
+
+    }
+}
